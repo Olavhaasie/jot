ed5dd66f0ced7901572f2be91cab07230c310358
Olav de Haas <o.n.dehaas@student.tudelft.nl>
2018-12-15 22:34:52 +0100

    add integration test for creating and inserting into a new journal

--- tests/cli_test.rs
+++ tests/cli_test.rs
@@ -1,9 +1,21 @@
-use std::process::Command;
 use assert_cmd::prelude::*;
+use std::process::Command;
 
 #[test]
 fn help_and_quit() {
     let mut cmd = Command::main_binary().unwrap();
     cmd.args(&["--help"]);
+    cmd.assert().success();
+}
+
+#[test]
+fn create_and_insert() {
+    let mut cmd = Command::main_binary().unwrap();
+    let file = tempfile::Builder::new()
+        .tempfile_in("./")
+        .expect("failed to create tempfile in current directory");
+    cmd.args(&["-d", file.path().to_str().unwrap()])
+        .with_stdin()
+        .buffer("test entry");
     cmd.assert().success();
 }
