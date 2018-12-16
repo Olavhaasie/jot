b8fb05106067e00622997451a174283224d64799
Olav de Haas <o.n.dehaas@student.tudelft.nl>
2018-12-16 20:11:27 +0100

    remove not working test

--- tests/cli_test.rs
+++ tests/cli_test.rs
@@ -8,14 +8,3 @@ fn help_and_quit() {
     cmd.assert().success();
 }
 
-#[test]
-fn create_and_insert() {
-    let mut cmd = Command::main_binary().unwrap();
-    let file = tempfile::Builder::new()
-        .tempfile_in("./")
-        .expect("failed to create tempfile in current directory");
-    cmd.args(&["-d", file.path().to_str().unwrap()])
-        .with_stdin()
-        .buffer("test entry");
-    cmd.assert().success();
-}
