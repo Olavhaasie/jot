70037134b33834f347437ed37debaf7a748ac2ec
Olav de Haas <o.n.dehaas@student.tudelft.nl>
2019-02-23 22:35:55 +0100

    fix assert_cmd test

--- tests/cli_test.rs
+++ tests/cli_test.rs
@@ -3,7 +3,7 @@ use std::process::Command;
 
 #[test]
 fn help_and_quit() {
-    let mut cmd = Command::main_binary().unwrap();
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
     cmd.args(&["--help"]);
     cmd.assert().success();
 }
